02f9dd3a9deee9cdab4999c3c49c328af37ac47b
RajshreeX <mailforrajshree@gmailcom>
2026-01-18 01:06:44 +0530

    Daily DSA practice: 2026-01-18

---
+++ TwoPointer_SlidingWindow/SubstringK-distinct.cpp
@@ -0,0 +1,31 @@
+//Substrings with K Distinct: https://www.geeksforgeeks.org/problems/count-number-of-substrings4528/1
+
+//Sliding Window: 
+//1.I feed the array into a map
+//2. start shrinking window size from left, if freq of element on left becomes 0; i erase that element from map
+//3. i count all substring anding at right as right count=count+right-left+1
+//4. i return count
+//5. finally, i find req substring by Substrings with at most K distinct−Substrings with at most (K−1) distinct
+
+//Time: O(n)    Space: O(k)
+
+int countSubstrAtMost(string& s, int k) {
+    unordered_map<char, int> freq;
+    int left=0;
+    int count=0;
+
+    for(int right=0; right<s.size(); right++) {
+        freq[s[right]]++;
+        while(freq.size()>k) {
+            freq[s[left]]--;
+            if(freq[s[left]]==0) freq.erase(s[left]);
+            left++;
+        }
+        count+=right-left+1;
+    }
+    return count;
+}
+
+int countSubstr(string& s, int k) {
+    return countSubstrAtMost(s,k) - countSubstrAtMost(s, k-1);
+}
