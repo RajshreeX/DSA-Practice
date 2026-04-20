2baf29ee9fef602d848e095f038b05650606467e
R E X I <154262369+RajshreeX@users.noreply.github.com>
2026-04-20 21:32:50 +0530

    Implement selection sort algorithm

---
+++ Array/SelectionSort.cpp
@@ -0,0 +1,14 @@
+void selectionSort(vector<int> &arr) {
+        // code here
+        int i, j, min;
+        i=0;
+        for(int i=0; i<arr.size(); i++) {
+            min=i;
+            for (int j=i+1; j<arr.size(); j++) {
+                if (arr[min]>arr[j]) {
+                    min=j;
+                }
+            }
+            swap(arr[min], arr[i]);
+        }
+    }
