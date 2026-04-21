1b47a1a4ce9aee05ed9a715e714477329979b55c
R E X I <154262369+RajshreeX@users.noreply.github.com>
2026-04-21 13:52:29 +0530

    21.04.26
    Implement bubble sort algorithm to sort an array.

---
+++ Array/BubbleSort.cpp
@@ -0,0 +1,13 @@
+void bubbleSort(vector<int>& arr) {
+        // code here
+        for(int i=0; i<arr.size(); i++) {
+            for(int j=i+1; j<arr.size(); j++) {
+                if(arr[i]>arr[j]) {
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }    
+        
+    }
